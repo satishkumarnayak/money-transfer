2e4b34dcb59f7ab3a7d65dcbe992c3172f5e6058
Satish Nayak <saishkumarNayak@gmail.com>
2019-07-14 11:17:00 +0530

    Initial Setup

---
+++ src/main/java/com/instanceofcake/revolut/rest/App.java
@@ -0,0 +1,13 @@
+package com.instanceofcake.revolut.rest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
